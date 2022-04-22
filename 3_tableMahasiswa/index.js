7aa4f400be2cdc2ce7bd0f0edbd326d8559e7a5b
Boreneoux <ichlasul.ap@gmail.com>
2022-04-22 14:40:29 +0700

    Added Create/Input Function

--- 3_tableMahasiswa/index.js
+++ 3_tableMahasiswa/index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const dataMahasiswa = require('./data.json')
+// const dataMahasiswa = require('./data.json')
+// let Mahasiswa = dataMahasiswa;
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+let Mahasiswa = [
+    {
+        NIM: "20200801111",
+        name: "Ichlasul Fikri",
+        nilai: "88",
+        grade: "",
+        status: ""
+    },
+    {
+        NIM: "20200801112",
+        name: "Eko Musk",
+        nilai: "99",
+        grade: "",
+        status: ""
+    },
+    {
+        NIM: "20200801113",
+        name: "Jeff Sutarman",
+        nilai: "50",
+        grade: "",
+        status: ""
+    }
+]
 
 
 app.get('/', (req, res) => {
@@ -15,25 +41,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/view', (req, res) => {
-    let Mahasiswa = dataMahasiswa;
-    // let Mahasiswa = { ...data };
-    // for (let m of Mahasiswa) {
-    //     if (m.nilai >= 80 && m.nilai <= 100) {
-    //         m.grade = 'A'
-    //         m.status = 'LULUS!'
-    //     } else if (m.nilai >= 60 && m.nilai <= 79) {
-    //         m.grade = 'B'
-    //         m.status = 'LULUS!'
-    //     } else if (m.nilai >= 0 && m.nilai <= 59) {
-    //         m.grade = 'C'
-    //         m.status = 'GAGAL!'
-    //     }
-    //     // console.log(m.name)
-    // }
-    // console.log(Mahasiswa);
-    res.render('view', { ...Mahasiswa });
-    console.log(Mahasiswa);
+    res.render('view', { Mahasiswa });
 })
+
+app.get('/input', (req, res) => {
+    res.render('input')
+})
+
+app.post('/input', (req, res) => {
+    const { NIM, name, nilai } = req.body;
+    Mahasiswa.push({ NIM, name, nilai })
+    res.redirect('view')
+})
+
+// app.get('/view/:NIM', (req, res) => {
+//     const { NIM } = req.params;
+//     const student = Mahasiswa.find(m => m.NIM === parseInt(NIM))
+//     res.render('show', { student })
+// })
+
+
 
 app.listen(3000, () => {
     console.log('Listening On Port 3000')
