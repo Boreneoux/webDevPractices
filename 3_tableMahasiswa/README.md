ae0ce3d236c12c57620089715f6e79233a9ecce6
Boreneoux <ichlasul.ap@gmail.com>
2022-04-23 14:58:37 +0700

    Added Edit / Delete + end of the project + README.md

---
+++ 3_tableMahasiswa/README.md
@@ -0,0 +1,20 @@
+# Table Mahasiswa
+Sebuah WebApp untuk saya latihan materi Express & RESTful Routes pada saat mengikuti course [The Web Developer Bootcamp di Udemy.](https://www.udemy.com/course/the-web-developer-bootcamp/)
+
+WebApp ini dapat memvalidasi nilai mahasiswa untuk mengetahui apakah mahasiswa tersebut lulus atau gagal pada matakuliahnya.
+
+## Yang digunakan pada project ini 
+- [ExpressJS](https://expressjs.com/)
+- [Bootstrap](https://getbootstrap.com/) 
+
+## Tampilan Project
+### Homepage :
+![homepage](/doc/homepage.png)
+### View Page:
+![view-page](/doc/view.png)
+### Input Page: 
+![input-page](/doc/input.png)
+### Show / Detail Page :
+![show-detail-page](/doc/show.png)
+### Edit Page:
+![edit-page](/doc/edit.png)
--- 3_tableMahasiswa/index.js
+++ 3_tableMahasiswa/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const dataMahasiswa = require('./data.json')
-// let Mahasiswa = dataMahasiswa;
+const methodOverride = require('method-override')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Untuk membuat patch & delete request
+app.use(methodOverride('_method'))
+// untuk parsing data form didalam request post pada body
 app.use(express.urlencoded({ extended: true }))
+// Untuk Parse data yang dikirim dari post request yang menggunakan format JSON
 app.use(express.json())
+// Untuk akses folder views dan setup untuk EJS
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+// fake database menggunakan array of object
 let Mahasiswa = [
     {
         NIM: "20200801111",
@@ -35,31 +40,59 @@ let Mahasiswa = [
     }
 ]
 
-
+// Homepage yang menampilkan halaman homepage.
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+// READ function/routes - View page yang menampilkan seluruh data mahasiswa yang ada.
 app.get('/view', (req, res) => {
     res.render('view', { Mahasiswa });
 })
 
+//Input page yang menampilkan form untuk memasukkan data mahasiwa baru.
 app.get('/input', (req, res) => {
     res.render('input')
 })
 
+// CREATE function/routes - untuk input data mahasiswa baru ke dalam object array Mahasiswa
 app.post('/input', (req, res) => {
     const { NIM, name, nilai } = req.body;
     Mahasiswa.push({ NIM, name, nilai })
     res.redirect('view')
 })
 
-// app.get('/view/:NIM', (req, res) => {
-//     const { NIM } = req.params;
-//     const student = Mahasiswa.find(m => m.NIM === parseInt(NIM))
-//     res.render('show', { student })
-// })
+// Show page or detail page untuk menampilkan 1 data mahasiswa tertentu. (Bagian dari READ juga(?))
+app.get('/show/:NIM', (req, res) => {
+    const { NIM } = req.params;
+    const student = Mahasiswa.find(m => m.NIM === NIM)
+    res.render('show', { student })
+})
 
+// Edit page untuk menampilkan page yang digunakan untuk mengubah nilai mahasiswa tertentu.
+app.get('/edit/:NIM', (req, res) => {
+    const { NIM } = req.params;
+    const student = Mahasiswa.find(m => m.NIM === NIM)
+    res.render('edit', { student })
+
+})
+
+// EDIT function/routes - untuk mengubah data mahasiswa tertentu dengan data baru yang baru saja di input.
+app.patch('/edit/:NIM', (req, res) => {
+    const { NIM } = req.params;
+    const newNilai = req.body.nilai;
+    const foundStudent = Mahasiswa.find(m => m.NIM === NIM)
+    foundStudent.nilai = newNilai;
+    res.redirect('/view')
+
+})
+
+// DELETE function/routes - untuk menghapus atau menghancurkan data mahasiswa tertentu.
+app.delete('/edit/:NIM', (req, res) => {
+    const { NIM } = req.params;
+    Mahasiswa = Mahasiswa.filter(m => m.NIM !== NIM);
+    res.redirect('/view')
+})
 
 
 app.listen(3000, () => {
